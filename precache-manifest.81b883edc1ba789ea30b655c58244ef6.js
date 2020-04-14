ed7ed1fb6a7e9a36aab27e9b5e2d7f4c8e99d906
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2020-04-14 10:29:17 +1000

    Updates

---
+++ precache-manifest.81b883edc1ba789ea30b655c58244ef6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3758d59a819641abfb1fdb2f786179",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cdf15ceff57f63789ce0",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fc2e7835278a74d46195",
+    "url": "/robofriends/static/css/main.da4d11cd.chunk.css"
+  },
+  {
+    "revision": "cdf15ceff57f63789ce0",
+    "url": "/robofriends/static/js/2.34f3f7f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.34f3f7f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2e7835278a74d46195",
+    "url": "/robofriends/static/js/main.3a2d2976.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  },
+  {
+    "revision": "2d159419ec2ee0ed7a6fa470dd7fba92",
+    "url": "/robofriends/static/media/Gotu-Regular.2d159419.ttf"
+  },
+  {
+    "revision": "692c37377cd1e0f4850f47c7c0148426",
+    "url": "/robofriends/static/media/VT323-Regular.692c3737.ttf"
+  }
+]);
